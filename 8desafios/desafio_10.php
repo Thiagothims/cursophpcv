0f9fda00f8b4f783e836856e1257dbaf90668025
Thiagothims <wwthiagothims@gmail.com>
2023-05-11 09:38:17 -0300

    Primeiras aulas

--- 8desafios/desafio_10.php
+++ 8desafios/desafio_10.php
@@ -10,13 +10,12 @@
 </head>
 
 <body>
-  <?php 
+  <?php
   $anoNasc = $_GET["anoNasc"] ?? 2000;
-  $anoAtual = $_GET["anoAtual"] ?? date("Y"); 
-  
-  // $anoAtual = date("Y");      // ano atual por função, fazer como acima, colocando a data atual como opção caso não receba o ano pelo input              
+  // $ano = $_GET["ano"] ?? date("Y"); // ou
 
-  $idade = $anoAtual - $anoNasc;
+  $anoAtual = date("Y");
+  $ano = $_GET["ano"] ?? $anoAtual;
 
   ?>
   <h1>Desafio 10</h1>
@@ -24,17 +23,28 @@
     <h2>Calculando idades</h2>
     <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
       <label for="anoNasc">Ano de Nascimento</label>
-      <input type="number" name="anoNasc" id="anoNasc" max="2100" min="1850" value="<?= $anoNasc ?>">
-      <label for="anoAtual">Ano Atual ou Escolhido</label>
-      <input type="number" name="anoAtual" id="anoAtual" max="2100" min="1850" value="<?= $anoAtual ?>">
-        <h6>*Padrão: ano atual 2023</h6>
+      <input type="number" name="anoNasc" id="anoNasc" max="<?= $anoAtual ?>" min="1850" value="<?= $anoNasc ?>">
+      <label for="ano">Ano Atual ou Escolhido</label>
+      <input type="number" name="ano" id="ano" max="2100" min="1850" value="<?= $ano ?>">
+      <h6>*Padrão: ano atual <?= $anoAtual ?></h6>
       <input type="submit" value="Verificar">
     </form>
     <a href="./index.html"><button>Voltar</button></a>
   </main>
   <section>
-    <h2>Resultado</h2>
-    <?= "<p>No ano de <strong>$anoAtual</strong>, nascidos em<strong> $anoNasc</strong> tem ou terão<strong> $idade</strong> de idade.</p>"?>
+    <?php
+    $idade = $ano - $anoNasc;
+
+    if ($anoNasc > $ano) {
+      echo "<h2>Verifique</h2>";
+      echo "<p>O ano de nascimento não pode ser menor que o ano atual<p>";
+      echo "<h3>Em $ano você ainda não era nascido!</h3>";
+    } else {
+      echo "<h2>Resultado</h2>";
+      echo "<p>No ano de <strong>$ano</strong>, nascidos em<strong> $anoNasc</strong> tem ou terão<strong> $idade</strong> anos(s) de idade.</p>";
+    }
+    ?>
+    
   </section>
 </body>
 
